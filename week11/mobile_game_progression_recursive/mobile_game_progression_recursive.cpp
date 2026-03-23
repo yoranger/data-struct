fcc25873f34f4f87a2d86fc9b9f28b0d04d9bb4a
yoranger <yoranger0123@gmail.com>
2026-03-23 23:12:12 +0800

    Update mobile_game_progression_recursive.cpp

--- week11/mobile_game_progression_recursive/mobile_game_progression_recursive.cpp
+++ week11/mobile_game_progression_recursive/mobile_game_progression_recursive.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
 int calculateProgressionPaths(int n) {
-    int totalVal = 0;
-    if(n > 1)
-    {
-        totalVal += calculateProgressionPaths(n - 1);
-        totalVal += calculateProgressionPaths(n - 2);
-    }
-    else if(n != 0)
-    {
-        ++totalVal;
-    }
-    if(n == 0)
-        ++totalVal;
+    if (n == 1)
+        return 1;
+    if (n == 2)
+        return 2;
 
-    return totalVal;
+    return calculateProgressionPaths(n - 1) + calculateProgressionPaths(n - 2);
+
+    // int totalVal = 0;
+    // if(n > 1)
+    // {
+    //     totalVal += calculateProgressionPaths(n - 1);
+    //     totalVal += calculateProgressionPaths(n - 2);
+    // }
+    // else if(n != 0)
+    // {
+    //     ++totalVal;
+    // }
+    // if(n == 0)
+    //     ++totalVal;
+
+    // return totalVal;
 }
